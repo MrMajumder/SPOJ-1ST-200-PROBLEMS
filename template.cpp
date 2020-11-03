fa92540813a7759094c7c4b8a6bff25ecca0fad6
Shafayat Hossain Majumder <40597489+MrMajumder@users.noreply.github.com>
2020-11-03 22:53:22 +0600

    Added template

---
+++ template.cpp
@@ -0,0 +1,54 @@
+/*
+    Shafayat Hossain Majumder
+    This is my boiler plate code, I use it for cp, and other stuffs.
+    I've been continuously updating this template since the start.
+    (Date today: 03 Nov, 2020)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//defined my datatypes
+typedef long double         ld;
+typedef long long           ll;
+typedef unsigned long long  ull;
+typedef pair<int, int>      pii;
+typedef pair<ll, ll>        pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>>  vvll;
+
+//defined my macro functions
+#define all(x)              (x).begin(),(x).end()
+#define pb                  push_back
+#define printclock          cerr<<"Time : "<<1000*(ld)clock()/(ld)CLOCKS_PER_SEC<<"ms\n"
+#define NITRO               ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+//defined my macro strings
+#define ENDL  "\n"
+#define gap    " "
+#define ha   "YES"
+#define na    "NO"
+
+//defined my constants
+const int    INF = 0x3f3f3f3f;              //1061109567, > 1e9
+const ll     LNF = 0x3f3f3f3f3f3f3f3f;      //4557430888798830399, > 4e18
+const int    MOD = 1000000007;              //9 digits, 1e9 + 7
+const double EPS = 1e-8;
+const ld     EUL = 2.71828182845904523536;  //20digits
+const ld     PII = 3.14159265358979323846;  //20digits
+//int   = 4e9,     ll = 1e19   hi value
+//float = 7digits, double = 15digits, ld = 21 digits precision
+
+int main()
+{
+    NITRO;
+    /*
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    */
+
+    printclock;
+    return 0;
+}
